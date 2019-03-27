d65e6089540ba74ac85abc97ccac38512a928785
Thomas Johnson <30642671+tomajohnson21@users.noreply.github.com>
2019-03-26 19:43:06 -0500

    Fixed previous merge

--- models/event.js
+++ models/event.js
@@ -1,20 +1,16 @@
 module.exports = function(sequelize, DataTypes) {
-  var musEvent = sequelize.define("musEvent", {
+  var Event = sequelize.define("Event", {
     title: DataTypes.STRING,
     genre: DataTypes.STRING,
-    //date: DataTypes.SRING,
+    date: DataTypes.STRING,
+    total_slots: DataTypes.INTEGER,
+    filled_slots: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
     location: DataTypes.STRING,
     description: DataTypes.TEXT
   });
 
-
-  // Event.associate =  function(models){
-
-  //   Event.hasMany(models.Artist, {
-  //     onDelete: "cascade"
-  //   });
-  // };
-
-  return musEvent;
-
+  return Event;
 };
