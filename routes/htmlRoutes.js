89e9fe6c037cbe30d7cb80768b181db3d7c5716b
Miles Kenner <mdk0109@yahoo.com>
2019-03-26 19:43:49 -0500

    not broken anymore

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -28,6 +28,10 @@ module.exports = function(app) {
     })
   })
 
+  app.get("/login", function(req, res){
+    res.render("login")
+  })
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
