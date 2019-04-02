9c146894d630ebc2b3426c6c75c30cdab1a0e1e4
Miles Kenner <mdk0109@yahoo.com>
2019-04-01 20:50:45 -0500

    couple of form styling changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -54,11 +54,11 @@ module.exports = function(app) {
     })
   });
  
-  app.get("/login", function(req, res){
-    res.render("login", {
-      style: "styles.css"
-    })
-  })
+  // app.get("/login", function(req, res){
+  //   res.render("login", {
+  //     style: "styles.css"
+  //   })
+  // })
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
