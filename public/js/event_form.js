ae7360439849b1c5be840e6f99fd2069f1ac06a1
Tom Johnson <tomajohnson21@gmail.com>
2019-04-02 12:15:55 -0500

    Updated event form to include street address, city, and state

--- public/js/event_form.js
+++ public/js/event_form.js
@@ -2,7 +2,9 @@ var validateForm = function(){
 
     //Variables for all form fields
     var title = $("#event-title").val().trim();
-    var location = $("#event-location").val().trim();
+    var address = $("#event-address").val().trim();
+    var city = $("#event-city").val().trim();
+    var state = $("#event-state").val().trim();
     var date = $("#event-date").val().trim();
     var genre = $("#event-genre").val().trim();
     var description = $("#event-description").val().trim();
@@ -10,7 +12,9 @@ var validateForm = function(){
 
     //Variables for validation indicators
     var title_indicator = $("#title-invalid");
-    var location_indicator = $("#location-invalid");
+    var address_indicator = $("#address-invalid");
+    var city_indicator = $("#city-invalid");
+    var state_indicator = $("#state-invalid")
     var date_indicator = $("#date-invalid");
     var genre_indicator = $("#genre-invalid");
     var description_indicator = $("#description-invalid");
@@ -18,7 +22,9 @@ var validateForm = function(){
 
     //Boolean variables for determining if the form is valid
     var title_valid = false;
-    var location_valid = false;
+    var address_valid = false;
+    var city_valid = false;
+    var state_valid = false;
     var date_valid = false;
     var genre_valid = false;
     var description_valid = false;
@@ -31,16 +37,32 @@ var validateForm = function(){
         title_valid = true;
     }
 
-    if(!location){
-        location_indicator.show();
+    if(!address){
+        address_indicator.show();
     } else {
-        location_indicator.hide();
-        location_valid = true;
+        address_indicator.hide();
+        address_valid = true;
+    }
+
+    if(!city){
+        city_indicator.show();
+    } else {
+        city_indicator.hide();
+        city_valid = true;
+    }
+
+    if(!state){
+        state_indicator.show();
+    } else if(!validateState(state)){
+        state_indicator.show();
+    } else {
+        state_indicator.hide();
+        state_valid = true;
     }
 
     if(!date){
         date_indicator.show();
-    } else if(!validDate(date)){
+    } else if(!validateDate(date)){
         date_indicator.show()
     } else {
         date_indicator.hide();
@@ -68,27 +90,38 @@ var validateForm = function(){
         openings_valid = true;
     }
 
-    if(title_valid && location_valid && date_valid && genre_valid && description_valid && openings_valid){
+    if(title_valid && address_valid && date_valid && genre_valid && description_valid && openings_valid && state_valid && city_valid){
 
         var event_data = {
             title: title,
-            location: location,
+            street_address: address,
+            state: state,
+            city: city,
             date: date,
             genre: genre,
             description: description,
             total_slots: openings
         }
 
-        postEvent(event_data)
+        postEvent(event_data);
+    } 
+}
+var validateState = function(state){
+    var stateArr = ['AL', 'AK', 'AZ', 'AR', 'CO', 'CT', 'DC', 'DE', 'FL', 
+                    'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 
+                    'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 
+                    'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 
+                    'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 
+                    'VA', 'WA', 'WV', 'WI', 'WY']
 
+    if(stateArr.includes(state)){
         return true;
-    } else {
-
-        return false;
     }
+
+    return false;
 }
 
-var validDate = function(new_date) {
+var validateDate = function(new_date) {
 
     date_format = "YYYY-MM-DD";
     
@@ -134,7 +167,8 @@ var postEvent = function(new_event) {
         url: "/api/events",
         data: new_event
     }).then(function(response){
-        console.log(response)    
+        console.log(response)  
+        goBackHome();  
     })
 }
 
@@ -154,13 +188,13 @@ function clearForm() {
 $("#event-submit").on("click", function(){
 
     event.preventDefault();
-    if(validateForm()){
-        goBackHome();
-    }
+    validateForm();
 });
 
 $("#title-invalid").hide();
-$("#location-invalid").hide();
+$("#address-invalid").hide();
+$("#city-invalid").hide();
+$("#state-invalid").hide();
 $("#date-invalid").hide();
 $("#genre-invalid").hide();
 $("#description-invalid").hide();
